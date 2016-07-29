2efc5e329338a89ff95a3c1702a6877de7d377d7
Chris Withers <chris@simplistix.co.uk>
2016-07-29 07:02:34 +0100

    Prepare for 0.2.0 release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sockpuppet',
-    version='0.1.0',
+    version='0.2.0',
     author='Chris Withers',
     author_email='chris@withers.org',
     license='MIT',
