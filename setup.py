3e4211e63ccc45586e78b8a570f70e12db6fc902
Chris Withers <chris@withers.org>
2019-01-09 15:04:25 +0000

    fix tag releaser and prepare for 0.2.2 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sockpuppet',
-    version='0.2.1',
+    version='0.2.2',
     author='Chris Withers',
     author_email='chris@withers.org',
     license='MIT',
